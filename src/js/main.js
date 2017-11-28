75c00a55616adc2d6b50a991169873ea3c5a8f92
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2017-11-28 12:57:31 +0200

    bluesite.com init

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+/*
+ Third party
+ */
+
+//= ../../bower_components/jquery/dist/jquery.min.js
+//= ../../bower_components/materialize/dist/js/materialize.min.js
+
+/*
+    Custom
+ */
+//= partials/common.js
